5d05f2775837739c36e38c2d1e94efcaf3136773
tal1988 <tb_bit@walla.com>
2017-04-25 20:30:28 +0300

    User schema

---
+++ models/user.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+var md5 = require('MD5');
+
+var userSchema = mongoose.Schema({
+    name: String,
+    desc: String,
+    username: String,
+    password: String,
+    email: String,
+    avatar: String
+});
+
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
