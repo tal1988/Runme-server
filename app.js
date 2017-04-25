f972345a7512b596ebc233d35147979c79341371
tal1988 <tb_bit@walla.com>
2017-04-25 20:29:15 +0300

    DB INIT

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+require("dotenv").load();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
+var passport = require('passport');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+mongoose.connect(process.env.MONGOLAB_URI, function(err)
+{
+    if (err) throw err;
+    else console.log("Connected To The DB");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,19 +30,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
 
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next)
+{
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next)
+{
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -44,3 +62,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+console.log("http://localhost:3000");
